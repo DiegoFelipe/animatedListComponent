392799f590a8ebebf7c5da4a0b2ab833621810e0
Diego Felipe <diegofelipe.14@gmail.com>
2019-06-16 18:20:35 -0400

    update

--- index.js
+++ index.js
@@ -11,24 +11,23 @@ var _Index = _interopRequireDefault(require("./Index.css"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var animatedlist = function animatedlist(props) {
-  handleClick = function handleClick(_) {
+var Animatedlist = function Animatedlist(props) {
+  var handleClick = function handleClick(_) {
     var list = document.getElementById('animated-list-comp');
     var newLI = document.createElement('li');
     newLI.innerHTML = 'A new item';
     list.appendChild(newLI);
+    console.log('wowowowo');
   };
 
   return _react["default"].createElement("div", null, _react["default"].createElement("ul", {
     id: "animated-list-comp",
-    "class": "swing"
+    className: "swing"
   }), _react["default"].createElement("button", {
     id: "animated-list-component-add-button",
-    onChange: function onChange() {
-      return handleClick();
-    }
-  }, props.button - text));
+    onClick: handleClick
+  }, props.buttonText));
 };
 
-var _default = animatedlist;
+var _default = Animatedlist;
 exports["default"] = _default;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import React from 'react'
 import Index from './Index.css'
 
-const animatedlist = props => {
+const Animatedlist = props => {
 
-    handleClick = _ => {
+    const handleClick = _ => {
 
         var list = document.getElementById('animated-list-comp');
         var newLI = document.createElement('li');
         newLI.innerHTML = 'A new item';
         list.appendChild(newLI);
+        console.log('wowowowo')
 
     }
 
-    
         return ( 
             <div>
-                <ul id="animated-list-comp" class="swing">
+                <ul id="animated-list-comp" className="swing">
                 </ul>
-                <button id="animated-list-component-add-button" onChange={() => handleClick()}>{props.button-text}</button>
+                <button id="animated-list-component-add-button" onClick={handleClick}>{props.buttonText}</button>
             </div>
         )
-    
+   
 }
 
-export default animatedlist
+export default Animatedlist
